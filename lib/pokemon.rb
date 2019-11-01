5e76e0766d272d3ab1664da247676db53182f3b1
Christian Perez <perezchristian1012@gmail.com>
2019-11-01 01:30:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,14 @@
 class Pokemon
 
+<<<<<<< HEAD
   attr_accessor :id, :name, :type, :db
 
   def initialize(id:, name:, type:, db:)
+=======
+  attr_accessor id:, name:, type:, db:)
+
+  def initialize(pokemon)
+>>>>>>> e5079f983132683a72fd5e14e8fe42dfe5fbcebc
     @id = id
     @name = name
     @type = type
@@ -14,7 +20,11 @@ class Pokemon
   end
 
   def self.find(id, db)
+<<<<<<< HEAD
     found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+=======
+    found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+>>>>>>> e5079f983132683a72fd5e14e8fe42dfe5fbcebc
     Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db)
   end
 end
